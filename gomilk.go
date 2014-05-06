ca87b61a527fd7f9e0f658bfac00388547c253d3
ongaeshi <ongaeshi0621@gmail.com>
2014-05-07 00:01:49 +0900

    Use pt for test

--- gomilk.go
+++ gomilk.go
@@ -2,12 +2,28 @@ package main
  
 import (
 	"fmt"
+	flags "github.com/jessevdk/go-flags"
+	"github.com/monochromegane/terminal"
+	"github.com/monochromegane/the_platinum_searcher/search"
+	"github.com/monochromegane/the_platinum_searcher/search/option"
+	// "github.com/ongaeshi/gomilk/search"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+	"runtime"
 	"strings"
 )
+
+const version = "0.1.0"
+
+func init() {
+	if cpu := runtime.NumCPU(); cpu == 1 {
+		runtime.GOMAXPROCS(2)
+	} else {
+		runtime.GOMAXPROCS(cpu)
+	}
+}
 
 func readURL(url string) (string, error) {
 	response, err := http.Get(url)
@@ -36,4 +52,66 @@ func main() {
 	}
 	
 	fmt.Println(contents)
+
+	// test
+	var opts option.Option
+
+	parser := flags.NewParser(&opts, flags.Default)
+	parser.Name = "gomilk"
+	parser.Usage = "[OPTIONS] PATTERN [PATH]"
+
+	args, err := parser.Parse()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if opts.Version {
+		fmt.Printf("%s\n", version)
+		os.Exit(0)
+	}
+
+	if len(args) == 0 && opts.FilesWithRegexp == "" {
+		parser.WriteHelp(os.Stdout)
+		os.Exit(1)
+	}
+
+	var root = "."
+	if len(args) == 2 {
+		root = strings.TrimRight(args[1], "\"")
+		_, err := os.Lstat(root)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			os.Exit(1)
+		}
+	}
+
+	opts.Proc = runtime.NumCPU()
+
+	if !terminal.IsTerminal(os.Stdout) {
+		opts.NoColor = true
+		opts.NoGroup = true
+	}
+
+	if opts.Context > 0 {
+		opts.Before = opts.Context
+		opts.After = opts.Context
+	}
+
+	if opts.Context > 0 {
+		opts.Before = opts.Context
+		opts.After = opts.Context
+	}
+
+	pattern := ""
+	if len(args) > 0 {
+		pattern = args[0]
+	}
+
+	searcher := search.Searcher{root, pattern, &opts}
+	err = searcher.Search()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+
 }
